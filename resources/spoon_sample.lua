f2ccbb75ea894ac0966f0b801f8fc1921db70b2f
virgilsisoe <virgilsisoe@gmail.com>
2022-10-23 11:29:00 -0400

    fix template

--- resources/spoon_sample.lua
+++ resources/spoon_sample.lua
@@ -8,8 +8,8 @@ obj.__index = obj
 
 -- Metadata
 obj.name = "_spoonName_"
-obj.version = "0.0.1"
-obj.author = "_author_ <email>"
+obj.version = "0.1.0"
+obj.author = "<_author_@email>"
 obj.homepage = ""
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
