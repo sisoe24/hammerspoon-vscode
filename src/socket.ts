5bd8fb770b6ebed1796960c964a6dddb23fba103
virgilsisoe <virgilsisoe@gmail.com>
2022-10-23 14:44:18 -0400

    debug network into own output window

--- src/socket.ts
+++ src/socket.ts
@@ -10,6 +10,18 @@ const statusBarItem = vscode.window.createStatusBarItem();
 const hsDisconnect = "$(debug-disconnect) Hammerspoon: Disconnected";
 const hsConnect = "$(vm-connect) Hammerspoon: Listening...";
 
+const outputWindowNetwork = vscode.window.createOutputChannel("Hammerspoon Network");
+
+/**
+ * Append text to vscode console
+ *
+ * @param text text to append in console
+ */
+export function debugNetwork(text: string): void {
+    const date = new Date().toISOString();
+    outputWindowNetwork.appendLine(`[${date}] ${text}`);
+}
+
 /**
  * Create the status bar action.
  */
@@ -22,9 +34,12 @@ export function createStatusBar() {
 
 function executeVscodeCommand(command: string) {
     vscode.commands.executeCommand(command).then(
-        () => {},
+        (resolve) => {
+            debugNetwork("Command executed successfully");
+        },
         (reject) => {
-            vscode.window.showErrorMessage(reject.message);
+            debugNetwork(`Command rejected: ${reject.message}`);
+            vscode.window.showWarningMessage(reject.message);
         }
     );
 }
@@ -32,26 +47,32 @@ function executeVscodeCommand(command: string) {
 function startServer() {
     // TODO: save port value in settings
     const port = hammerspoonConfig("network.port") as number;
+
     statusBarItem.tooltip = `Toggle connection for Hammerspoon on localhost:${port}`;
+
     // TODO: add custom host
     server.listen(port);
 
+    debugNetwork(`Connecting to 'localhost:${port}'`);
     server.on("connection", function (socket) {
         socket.on("data", function (chunk) {
-            executeVscodeCommand(chunk.toString());
+            const data = chunk.toString();
+            debugNetwork(`Data received: '${data}'`);
+            executeVscodeCommand(data);
         });
 
-        socket.on("end", function () {
-            vscode.window.showInformationMessage("Closing connection.");
+        socket.on("end", async function () {
+            debugNetwork("Closing client connection.");
         });
 
         socket.on("error", function (err) {
-            vscode.window.showErrorMessage(err.message);
+            debugNetwork(`Server error: ${err.message}`);
+            vscode.window.showWarningMessage(err.message);
         });
     });
 }
 
-export function connectHammerspoon() {
+export async function connectHammerspoon() {
     if (server.listening) {
         statusBarItem.tooltip = "";
         statusBarItem.text = hsDisconnect;
